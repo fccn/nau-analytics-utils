e539c2ade05deb1df091cb1b47c77b52eed1ec8c
Vitor Souza Piña <vitor.pina@glintt.com>
2026-01-09 12:38:58 +0000

    feature: adding como libs folder and nau_analytics_products_utils_lib

---
+++ common_libs/src/nau_analytics_data_product_utils_lib/config.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    s3_access_key: str
+    s3_secret_key: str
+    s3_endpoint: str
+    app_name: str
+    iceberg_catalog_uri: str
+    iceberg_catalog_user: str
+    iceberg_catalog_password: str
+    iceberg_catalog_warehouse: str
